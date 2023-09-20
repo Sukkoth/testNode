aaaf6a8ad5a74c0178f47b15bc74df9dd5ae33e9
Sukkoth <suukootj@gmail.com>
2023-09-20 14:57:15 +0300

    all end points added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello World!'
+        message: 'Hello World!',
     });
 });
 
-app.listen(process.env.APP_PORT, () =>
-    console.log(`Server running on port ${process.env.APP_PORT}`)
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+
+app.post('/message', (req, res) => {
+    console.log(req.body);
+    return res.json({
+        message: 'recieved',
+        name: req.body,
+    });
+});
+
+app.listen(process.env.APP_PORT || 7000, () =>
+    console.log(`Server running on port ${process.env.APP_PORT || 7000}`)
 );
